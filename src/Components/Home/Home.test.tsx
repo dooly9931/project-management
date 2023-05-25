e4c1f9c3ba5e4fd59c84b4e56e3731fe73ee4668
Kyoungseo <kyoungseo@hyperithm.com>
2023-05-25 15:25:23 +0900

    fix: fix lint warnings and errors

--- src/Components/Home/Home.test.tsx
+++ src/Components/Home/Home.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
 import Home from "./Home";
 
 describe("<Home />", () => {
